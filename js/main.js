342ac03fc2122a87ac44378683a49b2407386573
windikite <windikite@yahoo.com>
2022-07-18 17:30:04 -0400

    Delete js directory

--- js/main.js
+++
@@ -1,7 +0,0 @@
-//Animate Smooth Scroll
-$('#view-portfolio').on('click', function() {
-    const images = $('#images').position().top;
-    $('html, body').animate({
-        scrollTop:images
-    }, 900);
-});
